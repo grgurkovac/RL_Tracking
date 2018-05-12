3321f31c95d3894bc08072db13ad6bed939c5582
Grg <grgur.kovac@fer.hr>
2018-05-12 19:31:57 +0200

    train_list

--- Animator.py
+++ Animator.py
@@ -279,7 +279,7 @@ class Trainer():
 
 if __name__ == "__main__":
 
-    dataset = VOT2017("./Datasets/vot2017/")
+    dataset = VOT2017("./Datasets/vot2017/", test_split="train_list.txt", train_splite="train_list.txt")
     time_steps = 10
     batch_size = 5
     frames, annots = dataset.get_next_video_frames_and_annots(test=False)
